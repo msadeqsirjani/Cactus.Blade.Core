eac2f1e9abd2276b4143fa7ef9a71e94bb7865c9
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-17 12:53:00 +0330

    Create Int32.Years.cs

---
+++ Core/System.Int32/Generic/Int32.Years.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static partial class Extension
+{
+    /// <summary>
+    ///     An Int32 extension method that years the given this.
+    /// </summary>
+    /// <param name="this">The @this to act on.</param>
+    /// <returns>A TimeSpan.</returns>
+    public static TimeSpan Years(this short @this)
+    {
+        var leap = @this / 4;
+
+        var days = 365 * @this + leap;
+
+        return days.Days();
+    }
+}
