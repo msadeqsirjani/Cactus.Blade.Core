dd9a0bffaa4d7e4442ce46b1dbb20dd3984f5d57
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-22 12:10:53 +0330

    Create String.ToArray.cs

---
+++ Core/System.String/System.Linq/String.ToArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static partial class Extension
+{
+    /// <summary>
+    /// Creates a <see cref="T:System.Collections.Generic.List`1"/> from an <see cref="T:System.String"/>.
+    /// </summary>
+    /// 
+    /// <returns>
+    /// A <see cref="T:System.Collections.Generic.List`1"/> that contains elements from the input sequence.
+    /// </returns>
+    /// <param name="this">The <see cref="T:System.String"/> to create a <see cref="T:System.Collections.Generic.List`1"/> from.</param>
+    /// <exception cref="T:System.ArgumentNullException"><paramref name="this"/> is null.</exception>
+    public static List<char> ToList(this string @this)
+    {
+        if (@this == null)
+            throw new ArgumentNullException(nameof(@this));
+
+        return new List<char>(@this.ToCharArray());
+    }
+}
