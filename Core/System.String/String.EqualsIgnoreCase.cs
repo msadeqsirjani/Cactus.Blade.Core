adc8bb03222cbc03e81cd58bf0a4b794c1477e35
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-22 12:10:13 +0330

    Update String.EqualsIgnoreCase.cs

--- Core/System.String/String.EqualsIgnoreCase.cs
+++ Core/System.String/String.EqualsIgnoreCase.cs
@@ -14,4 +14,18 @@ public static partial class Extension
     {
         return @this.Equals(comparedString, StringComparison.OrdinalIgnoreCase);
     }
+
+    /// <summary>
+    /// A string extension method that checks if '@this' is equal to another string regardless of it's case.
+    /// </summary>
+    /// <param name="this">The @this to act on.</param>
+    /// <param name="comparedString">The compared string.</param>
+    /// <param name="comparison">String Comparison</param>
+    /// <returns>
+    /// true if it contains all values, otherwise false.
+    /// </returns>
+    public static bool EqualsComparison(this string @this, string comparedString, StringComparison comparison)
+    {
+        return @this.Equals(comparedString, comparison);
+    }
 }
