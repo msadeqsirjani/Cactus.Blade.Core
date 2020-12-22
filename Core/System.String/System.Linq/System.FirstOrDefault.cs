178bd9aeaee8074d1b4e7ea05d4eb1cc5d6b076c
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-22 12:10:48 +0330

    Create System.FirstOrDefault.cs

---
+++ Core/System.String/System.Linq/System.FirstOrDefault.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static partial class Extension
+{
+    /// <summary>
+    /// Returns the first character of a string, or a default value if the string is empty.
+    /// </summary>
+    /// 
+    /// <returns>
+    /// default(<see cref="T:System.Char"/>) if <paramref name="this"/> is empty; otherwise, the first character in <paramref name="this"/>.
+    /// </returns>
+    /// <param name="this">The <see cref="T:System.String"/> to return the first character of.</param>
+    /// <exception cref="T:System.ArgumentNullException"><paramref name="this"/> is null.</exception>
+    public static char FirstOrDefault(this string @this)
+    {
+        if (@this == null)
+            throw new ArgumentNullException(nameof(@this));
+
+        return @this.Length == 0
+            ? default
+            : @this[0];
+    }
+}
