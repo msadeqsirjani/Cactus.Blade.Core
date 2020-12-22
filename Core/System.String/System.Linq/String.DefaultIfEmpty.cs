279855348a7ae647a412d18270ac7c2ae353f951
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-22 12:10:26 +0330

    Create String.DefaultIfEmpty.cs

---
+++ Core/System.String/System.Linq/String.DefaultIfEmpty.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static partial class Extension
+{
+    /// <summary>
+    /// Returns the string.
+    /// </summary>
+    /// 
+    /// <returns>
+    /// The <paramref name="this"/>.
+    /// </returns>
+    /// <param name="this">The sequence to return.</param>
+    /// <exception cref="T:System.ArgumentNullException"><paramref name="this"/> is null.</exception>
+    public static string DefaultIfEmpty(this string @this)
+    {
+        if (@this == null)
+            throw new ArgumentNullException(nameof(@this));
+
+        return @this.IsEmpty() ? new string('\0', 1) : @this;
+    }
+}
