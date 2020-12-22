331ecc063b32cad2bf708446325a92c7198d53c8
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-22 12:10:24 +0330

    Create String.Count.cs

---
+++ Core/System.String/System.Linq/String.Count.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static partial class Extension
+{
+    /// <summary>
+    /// Returns the number of characters in a string.
+    /// </summary>
+    /// 
+    /// <returns>
+    /// The number of characters in the input string.
+    /// </returns>
+    /// <param name="this">A string that contains characters to be counted.</param>
+    /// <exception cref="T:System.ArgumentNullException"><paramref name="this"/> is null.</exception>
+    public static int Count(this string @this)
+    {
+        if (@this == null)
+            throw new ArgumentNullException(nameof(@this));
+        return @this.Length;
+    }
+}
