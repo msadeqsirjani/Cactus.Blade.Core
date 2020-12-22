eab8aba177f4023504002e22171671217a4fe104
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-22 12:10:35 +0330

    Create String.First.cs

---
+++ Core/System.String/System.Linq/String.First.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public static partial class Extension
+{
+    /// <summary>
+    /// Returns the first character of a string.
+    /// </summary>
+    /// 
+    /// <returns>
+    /// The first character in the specified string.
+    /// </returns>
+    /// <param name="this">The <see cref="T:System.String"/> to return the first character of.</param>
+    /// <exception cref="T:System.ArgumentNullException"><paramref name="this"/> is null.</exception>
+    /// <exception cref="T:System.InvalidOperationException">The source string is empty.</exception>
+    public static char First(this string @this)
+    {
+        if (@this.IsNullOrEmpty())
+            throw new InvalidOperationException($"The {nameof(@this)} string cannot be empty or null but is.");
+
+        return @this[0];
+    }
+}
