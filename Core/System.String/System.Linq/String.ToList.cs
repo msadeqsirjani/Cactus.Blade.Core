9ac003f0d12a7f443aff0bb2298352270ef5bf95
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-22 12:10:50 +0330

    Create String.ToList.cs

---
+++ Core/System.String/System.Linq/String.ToList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static partial class Extension
+{
+    /// <summary>
+    /// Creates an array from a <see cref="T:System.String"/>.
+    /// </summary>
+    /// 
+    /// <returns>
+    /// An array that contains the characters from the input string.
+    /// </returns>
+    /// <param name="this">An <see cref="T:System.String"/> to create an array from.</param>
+    /// <exception cref="T:System.ArgumentNullException"><paramref name="this"/> is null.</exception>
+    public static char[] ToArray(this string @this)
+    {
+        if (@this == null)
+            throw new ArgumentNullException(nameof(@this));
+
+        return @this.ToCharArray();
+    }
+}
