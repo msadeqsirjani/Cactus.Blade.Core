e53ccdc3825f5a3df1d30f75feb977be123fbede
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 14:08:17 +0330

    Update Double.InRange.cs

--- Core/System.Double/CoreObject/Double.InRange.cs
+++ Core/System.Double/CoreObject/Double.InRange.cs
@@ -7,7 +7,6 @@
     /// <param name="minimum">The minimum value.</param>
     /// <param name="maximum">The maximum value.</param>
     /// <returns>true if the value is between inclusively the minimum and maximum, otherwise false.</returns>
-    /// ###
     public static bool InRange(this double @this, double minimum, double maximum)
     {
         return @this.CompareTo(minimum) >= 0 && @this.CompareTo(maximum) <= 0;
