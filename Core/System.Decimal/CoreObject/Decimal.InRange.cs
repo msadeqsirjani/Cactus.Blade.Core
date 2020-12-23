6b19415aa457b308eabfaca87f7f17b644189c16
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 14:08:11 +0330

    Update Decimal.InRange.cs

--- Core/System.Decimal/CoreObject/Decimal.InRange.cs
+++ Core/System.Decimal/CoreObject/Decimal.InRange.cs
@@ -1,15 +1,14 @@
 ﻿public static partial class Extension
 {
     /// <summary>
-    ///     A T extension method that check if the value is between inclusively the minValue and maxValue.
+    ///     A T extension method that check if the value is between inclusively the minimum and maximum.
     /// </summary>
     /// <param name="this">The @this to act on.</param>
-    /// <param name="minValue">The minimum value.</param>
-    /// <param name="maxValue">The maximum value.</param>
-    /// <returns>true if the value is between inclusively the minValue and maxValue, otherwise false.</returns>
-    /// ###
-    public static bool InRange(this decimal @this, decimal minValue, decimal maxValue)
+    /// <param name="minimum">The minimum value.</param>
+    /// <param name="maximum">The maximum value.</param>
+    /// <returns>true if the value is between inclusively the minimum and maximum, otherwise false.</returns>
+    public static bool InRange(this decimal @this, decimal minimum, decimal maximum)
     {
-        return @this.CompareTo(minValue) >= 0 && @this.CompareTo(maxValue) <= 0;
+        return @this.CompareTo(minimum) >= 0 && @this.CompareTo(maximum) <= 0;
     }
 }
