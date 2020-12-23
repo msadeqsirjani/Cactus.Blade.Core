1a4f92a04de8694d81eb18b907a3b50e72e68c7f
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 14:07:55 +0330

    Update DateTime.InRange.cs

--- Core/System.DateTime/CodeObject/DateTime.InRange.cs
+++ Core/System.DateTime/CodeObject/DateTime.InRange.cs
@@ -1,17 +1,17 @@
 ﻿public static partial class Extension
 {
     /// <summary>
-    ///     A T extension method that check if the value is between inclusively the minValue and maxValue.
+    ///     A T extension method that check if the value is between inclusively the minimum and maximum.
     /// </summary>
     /// <param name="this">The @this to act on.</param>
-    /// <param name="minValue">The minimum value.</param>
-    /// <param name="maxValue">The maximum value.</param>
-    /// <returns>true if the value is between inclusively the minValue and maxValue, otherwise false.</returns>
+    /// <param name="minimum">The minimum value.</param>
+    /// <param name="maximum">The maximum value.</param>
+    /// <returns>true if the value is between inclusively the minimum and maximum, otherwise false.</returns>
     /// ###
     /// <typeparam name="T">Generic type parameter.</typeparam>
-    public static bool InRange(this System.DateTime @this, System.DateTime minValue,
-        System.DateTime maxValue)
+    public static bool InRange(this System.DateTime @this, System.DateTime minimum,
+        System.DateTime maximum)
     {
-        return @this.CompareTo(minValue) >= 0 && @this.CompareTo(maxValue) <= 0;
+        return @this.CompareTo(minimum) >= 0 && @this.CompareTo(maximum) <= 0;
     }
 }
