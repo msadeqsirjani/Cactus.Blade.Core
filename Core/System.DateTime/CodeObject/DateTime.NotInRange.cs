b50334400bab87c709375b6f7da965d7fbbdbffd
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 14:08:02 +0330

    Create DateTime.NotInRange.cs

---
+++ Core/System.DateTime/CodeObject/DateTime.NotInRange.cs
@@ -0,0 +1,17 @@
+﻿public static partial class Extension
+{
+    /// <summary>
+    ///     A T extension method that check if the value is not between inclusively the minimum and maximum.
+    /// </summary>
+    /// <param name="this">The @this to act on.</param>
+    /// <param name="minimum">The minimum value.</param>
+    /// <param name="maximum">The maximum value.</param>
+    /// <returns>true if the value is not between inclusively the minimum and maximum, otherwise false.</returns>
+    /// ###
+    /// <typeparam name="T">Generic type parameter.</typeparam>
+    public static bool NotInRange(this System.DateTime @this, System.DateTime minimum,
+        System.DateTime maximum)
+    {
+        return @this.CompareTo(minimum) < 0 && @this.CompareTo(maximum) > 0;
+    }
+}
